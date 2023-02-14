30b22d1af9016cd56413204e8da9766c481fd7c7
andrewvitrenko <avitrenko2004@gmail.com>
2023-02-14 17:32:44 +0100

    feat: added handler for request

---
+++ handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	json2 "encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+func HandleTime(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	var currentTime = time.Now()
+	var resp = make(map[string]string)
+	resp["time"] = currentTime.Format("2006-01-02T15:04:05Z")
+	var json, err = json2.Marshal(resp)
+
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+
+	w.Write(json)
+	return
+
+}
