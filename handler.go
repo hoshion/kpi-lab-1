5e6b21ab2024cf4a52e817eca8ddb18c013c001d
just-hoshinon <mwadman2018@gmail.com>
2023-02-18 15:25:35 +0200

    Revert "updated time format"
    This reverts commit 89989377d9a490f7a5f761963f99647a23fc1df2.

--- handler.go
+++ handler.go
@@ -13,7 +13,7 @@ func HandleTime(w http.ResponseWriter, r *http.Request) {
 
 	var currentTime = time.Now()
 	var resp = make(map[string]string)
-	resp["time"] = currentTime.Format(time.RFC3339)
+	resp["time"] = currentTime.Format("2006-01-02T15:04:05Z")
 	var json, err = json2.Marshal(resp)
 
 	if err != nil {
